d37180689b3d36cb1b98c0302a1aad060ccd01cc
caglatunc <caglatunc03@gmail.com>
2024-05-19 21:08:27 +0300

    Desc

--- eCommerceServer.Domain/Products/Product.cs
+++ eCommerceServer.Domain/Products/Product.cs
@@ -3,4 +3,7 @@
 namespace eCommerceServer.Domain.Products;
 public sealed class Product : Entity
 {
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+ 
 }
