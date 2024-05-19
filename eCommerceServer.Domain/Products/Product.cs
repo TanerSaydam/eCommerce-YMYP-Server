5d1b0e598f9931b5dff4cd3f680c30c6311c5941
caglatunc <caglatunc03@gmail.com>
2024-05-19 20:43:12 +0300

    Gönderim denemesi

--- eCommerceServer.Domain/Products/Product.cs
+++ eCommerceServer.Domain/Products/Product.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using eCommerceServer.Domain.Abstractions;
 
 namespace eCommerceServer.Domain.Products;
-internal class Product
+public sealed class Product : Entity
 {
 }
