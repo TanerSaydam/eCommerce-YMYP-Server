9cfadbe45ad27a72fe50e6f8fc2d035944efcc32
TanerSaydam <tanersaydam@gmail.com>
2024-05-28 20:26:27 +0300

    Company migration atıldı

--- src/eCommerceServer.Infrastructure/Migrations/20240528172608_company-etitysi-olusturuldu.Designer.cs
+++ src/eCommerceServer.Infrastructure/Migrations/20240528172608_company-etitysi-olusturuldu.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using eCommerceServer.Infrastructure.Context;
@@ -11,9 +12,11 @@ using eCommerceServer.Infrastructure.Context;
 namespace eCommerceServer.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240528172608_company-etitysi-olusturuldu")]
+    partial class companyetitysiolusturuldu
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/eCommerceServer.Infrastructure/Migrations/20240528172608_company-etitysi-olusturuldu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eCommerceServer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class companyetitysiolusturuldu : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "company",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "varchar(50)", nullable: false),
+                    TaxDepartment = table.Column<int>(type: "integer", maxLength: 11, nullable: false),
+                    TaxNumber = table.Column<string>(type: "text", nullable: false),
+                    address_country = table.Column<string>(type: "varchar(50)", nullable: false),
+                    address_city = table.Column<string>(type: "varchar(50)", nullable: false),
+                    address_town = table.Column<string>(type: "varchar(50)", nullable: false),
+                    address_street = table.Column<string>(type: "varchar(50)", nullable: false),
+                    address_full_address = table.Column<string>(type: "varchar(200)", nullable: false),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_company", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "company");
+        }
+    }
+}
